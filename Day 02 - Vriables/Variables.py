61dc091f1fba98390c96ecbd40b469f56da25350
Muhammad-rehan-hamza <rehanhamzaofficial@gmail.com>
2026-08-20 10:08:38 +0500

    Add files via upload

---
+++ Day 02 - Vriables/Variables.py
@@ -0,0 +1,27 @@
+print("Variables")
+
+name = "REHAN HAMZA" # string
+print (name)
+print(type(name))
+
+age = 20  # int
+print(age)
+print(type(age))
+
+age_1 = 20.8  # float
+print(age_1)
+print(type(age_1))
+
+print ("Name :" , name , "Age :", age , "Exact Age :", age_1 )
+
+is_ok = True
+print(is_ok)
+
+age_2 = 17
+print(age_2)
+
+myClass = 14
+print(myClass)
+
+name = input("Enter name")
+print (name)
