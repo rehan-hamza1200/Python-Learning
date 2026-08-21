2a773015113f665fa22939d12e6733c5256fb076
Muhammad-rehan-hamza <rehanhamzaofficial@gmail.com>
2026-08-22 00:17:32 +0500

    Day 03 - Python Data Types

---
+++ Day 03 - Dataypes/L_3_Datatypes.py
@@ -0,0 +1,32 @@
+# Day 3 - Python Data Types
+
+# String
+name = "Rehan Hamza"
+
+# Integer
+age = 20
+
+# Float
+height = 5.8
+
+# Boolean
+is_student = True
+
+# Printing values
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# Checking data types
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+
+#we can perform any operations of datatypes
+print (age + 1)
+print(height - 20)
+print(name + "master")
+print(name , "master")
